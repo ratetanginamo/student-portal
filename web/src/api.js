0c1a0b9c37acd135633643d3fb470e8a27c6b57e
ratetanginamo <168286942+ratetanginamo@users.noreply.github.com>
2025-08-29 17:52:50 +0800

    Create api.js

---
+++ web/src/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+const API = axios.create({ baseURL: 'http://localhost:8080/api' });
+API.interceptors.request.use((cfg) => {
+  const t = localStorage.getItem('token');
+  if (t) cfg.headers.Authorization = 'Bearer ' + t;
+  return cfg;
+});
+export default API;
